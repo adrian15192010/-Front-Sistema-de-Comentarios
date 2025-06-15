5f0353bc97058ec159b6c126798599f79051b5ee
Diego Gonzalez <adrian15192010@gmail.com>
2025-06-15 10:54:30 -0400

    awdasd

---
+++ src/app/pages/bandeja/bandeja.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bandeja',
+  imports: [],
+  templateUrl: './bandeja.component.html',
+  styleUrl: './bandeja.component.css'
+})
+export class BandejaComponent {
+
+}
