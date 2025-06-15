be6c7806d9da0b9780a0398c3a97ffe09df282b6
Diego Gonzalez <adrian15192010@gmail.com>
2025-06-15 12:27:48 -0400

    asda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BandejaComponent } from './pages/bandeja/bandeja.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BandejaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/bandeja/bandeja.component.ts
+++ src/app/pages/bandeja/bandeja.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-bandeja',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './bandeja.component.html',
   styleUrl: './bandeja.component.css'
 })
