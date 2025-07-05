27f8343e2727c1a8882db968d197785bb9cd99b2
Diego Gonzalez <adrian15192010@gmail.com>
2025-07-05 18:15:43 -0400

    sdfsdf

--- src/app/components/respuesta/respuesta.component.ts
+++ src/app/components/respuesta/respuesta.component.ts
@@ -1,4 +1,6 @@
 import { Component, input } from '@angular/core';
+import { RespuestaService } from '../../respuesta.service';
+import { LoginService } from '../../login.service';
 
 @Component({
   selector: 'app-respuesta',
@@ -9,5 +11,27 @@ import { Component, input } from '@angular/core';
 export class RespuestaComponent {
 
    respuesta = input<any>();
+   respuestas : any = [];
+
+   constructor(public respuestaService: RespuestaService, public loginService: LoginService){}
+
+
+  reaccionar(){
+    this.respuestaService.reaccionar(this.respuesta().id).subscribe({
+      next:(data)=>{
+         
+        this.respuesta().reaccionList.push(data)
+
+      },
+      error: (e)=>{
+
+         this.respuesta().reaccionList = this.respuesta().reaccionList.filter(
+        (reaccion: any) => reaccion.userId !== this.loginService.userId
+        );
+
+      }
+    })
+  }
+
 
 }
--- src/app/respuesta.service.ts
+++ src/app/respuesta.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginService } from './login.service';
 
@@ -10,6 +10,18 @@ export class RespuestaService {
   constructor(private http: HttpClient, public loginService: LoginService) { }
 
   token : any = "eyJhbGciOiJIUzM4NCJ9.eyJuYW1lIjoiZGllZ28iLCJzdWIiOiJhZHJpYW4xNTE5MjAxMEBnbWFpbC5jb20iLCJpYXQiOjE3NTA3MTAxMzUsImV4cCI6MTc1MTMxNDkzNX0.jTW19j0ImFWY3drlV_2xpT84G2kwraQ6fq5dWGSlkOZHggG5EDQV8SX7NrLFabkZ"
+
+
+  reaccionar(respuestaId: any){
+
+   console.log(this.loginService.toke) 
+    
+  const headers = new HttpHeaders().set('Authorization', `Bearer ${this.loginService.toke}`);
+
+  return this.http.post<any>(`http://localhost:8030/api/reaccion/create/toRespuesta?id_respuesta=${respuestaId}`,{},
+   {headers : headers });
+   
+  }
 
 
 }
